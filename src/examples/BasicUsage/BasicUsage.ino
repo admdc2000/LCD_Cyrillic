4d06afc35f900b1ed7c3b0b1006020b7d329a778
Developer <dev@os.net>
2025-10-19 19:20:34 +0300

    Improved glyph for letter Д and added usage examples

--- src/examples/BasicUsage/BasicUsage.ino
+++ src/examples/BasicUsage/BasicUsage.ino
@@ -1,16 +1,34 @@
 // BasicUsage example (save this file as UTF-8 WITHOUT BOM)
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <LCD_Cyrillic.h>
+#include <Arduino.h>            // Include Arduino core library
+#include <Wire.h>               // Include Wire library for I2C communication
+#include <LiquidCrystal_I2C.h>  // Include the LiquidCrystal_I2C library
+#include <LCD_Cyrillic.h>       // Include the Cyrillic support header
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // adjust address if needed
 
 void setup() {
-  lcd.init();
-  lcd.backlight();
-
-  lcd.clear();
-  LCD_Cyrillic::print(lcd, "Привет, Мир!\nФУФАЙКА Ы =)", 0, 0);
+  lcd.init();       // Initialize the LCD
+  lcd.backlight();  // Turn on the backlight
 }
 
-void loop() {}
+void loop() {
+  lcd.clear();
+  LCD_Cyrillic::print(lcd, "Привет Мир!", 2, 0); // Print Cyrillic text: X=0, Y=0
+  delay(2000);
+  
+  lcd.clear();
+  LCD_Cyrillic::print(lcd, "Как дела?"); // Print Cyrillic text: X=0, Y=0
+  delay(2000);
+
+  lcd.clear();
+  LCD_Cyrillic::print(lcd, "Хорошо!", 9, 1); // Print Cyrillic text: X=9, Y=1
+  delay(2000);
+
+  lcd.clear();
+  LCD_Cyrillic::print(lcd, "Теперь встречаем строку номер 2"); // Print long text: X=0, Y=0
+  delay(6000);
+
+  lcd.clear();
+  LCD_Cyrillic::print(lcd, "Новая строка?\nВозврат каретки!"); // Using newline for line break
+  delay(6000);
+}
